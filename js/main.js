9daaf0a25c9286b10cf581a226607f46387fc557
ryankgit <ryankirschcontact@gmail.com>
2021-01-29 13:12:51 -0600

    Adds FeatureLayer and visual variables

--- js/main.js
+++ js/main.js
@@ -1,16 +1,163 @@
-require(["esri/Map", "esri/views/MapView"], function (Map, MapView) {
+require(["esri/Map", "esri/layers/FeatureLayer", "esri/views/MapView"], function (Map, FeatureLayer, MapView) {
 
     // Do not require an API key when using select basemaps
     // see: https://developers.arcgis.com/javascript/latest/api-reference/esri-Map.html#basemap 
     
+    // create map
     const map = new Map({
-        basemap: "dark-gray"
+        basemap: "gray-vector"
     });
 
+    // set FeatureLayer visual variables
+    const colorVisVar = {
+        "type": "color",
+        "field": "CrimeCnt",
+        "valueExpression": null,
+        "stops": [{
+            "value": 0,
+            "color": [
+              255,
+              252,
+              212,
+              255
+            ],
+            "label": "< 0"
+          },
+          {
+            "value": 25,
+            "color": [
+              224,
+              178,
+              193,
+              255
+            ],
+            "label": null
+          },
+          {
+            "value": 50.8,
+            "color": [
+              193,
+              104,
+              173,
+              255
+            ],
+            "label": "50.8"
+          },
+          {
+            "value": 75.9,
+            "color": [
+              123,
+              53,
+              120,
+              255
+            ],
+            "label": null
+          },
+          {
+            "value": 101,
+            "color": [
+              53,
+              2,
+              66,
+              255
+            ],
+            "label": "> 101"
+          }
+        ]
+      };
+    
+      const sizeVisVar = {
+        "type": "size",
+        "field": "NarcoticsC",
+        "valueExpression": null,
+        "valueUnit": "unknown",
+        "minSize": {
+          "type": "size",
+          "valueExpression": "$view.scale",
+          "stops": [{
+              "value": 1128,
+              "size": 12
+            },
+            {
+              "value": 2256,
+              "size": 12
+            },
+            {
+              "value": 288896,
+              "size": 3
+            },
+            {
+              "value": 2311162,
+              "size": 3
+            },
+            {
+              "value": 97989703,
+              "size": 1.5
+            }
+          ]
+        },
+        "maxSize": {
+          "type": "size",
+          "valueExpression": "$view.scale",
+          "stops": [{
+              "value": 1128,
+              "size": 60
+            },
+            {
+              "value": 2256,
+              "size": 60
+            },
+            {
+              "value": 288896,
+              "size": 37.5
+            },
+            {
+              "value": 2311162,
+              "size": 37.5
+            },
+            {
+              "value": 97989703,
+              "size": 18.75
+            }
+          ]
+        },
+        "minDataValue": 0,
+        "maxDataValue": 378
+      };
+
+
+    // create simple render, set visual variables
+    const renderer = {
+        type: "simple",     // autocasts as new SimpleRenderer
+        symbol: {
+            type: "simple-marker",      // autocasts as new SimpleMarkerSymbol
+            outline: {      // autocasts as new SimpleLineSymbol
+                color: [128, 128, 128],
+                width: 0.5
+            }
+        },
+        label: "test",
+        // set color and
+        visualVariables: [colorVisVar, sizeVisVar]
+    };
+
+    // create FeatureLayer
+    const fl = new FeatureLayer({
+        url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Chicago_Crime_Tracts/FeatureServer/0",
+        outFields: ["*"],
+        renderer: renderer
+    }); 
+
+    // create MapView
     view = new MapView({
         container: "viewDiv",
         map: map,
         zoom: 10,
-        center: [-87.0, 41.0]
+        center: [-87.66453728281347, 41.840392306471315]
+    });
+
+    // add FeatureLayer fl to map when MapView is loaded
+    view.when(function() {
+        map.add(fl);
     });
 });
